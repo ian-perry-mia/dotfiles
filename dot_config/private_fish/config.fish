7bc8945ed32e59e3d8c5a9b391cc85f84097cef0
Ian Perry <ian.perry@avionics411.com>
2026-03-12 14:38:39 -0500

    Update .config/fish/config.fish

--- dot_config/private_fish/config.fish
+++ dot_config/private_fish/config.fish
@@ -1,6 +1,15 @@
 if status is-interactive
     if [ -n "$SSH_CLIENT" ]; or [ -n "$SSH_TTY" ]
+        set -gx ZELLIJ_AUTO_EXIT    "true"
+        set -gx ZELLIJ_AUTO_ATTACH  "true"
         eval (zellij setup --generate-auto-start fish | string collect)
     end
     starship init fish | source
+end
+
+# Replicate bash !!
+abbr -a !! --position anywhere --function last_history_item
+
+function last_history_item
+    echo $history[1]
 end
