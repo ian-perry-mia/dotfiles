1d99303526452e969ea90adb4a543d4bc455b71c
Ian Perry <ian.perry@avionics411.com>
2026-03-13 07:44:49 -0500

    Update .config/fish/config.fish

--- dot_config/private_fish/config.fish
+++ dot_config/private_fish/config.fish
@@ -1,10 +1,17 @@
 if status is-interactive
+    # If SSH'd in, use zellij
     if [ -n "$SSH_CLIENT" ]; or [ -n "$SSH_TTY" ]
         set -gx ZELLIJ_AUTO_EXIT    "true"
         set -gx ZELLIJ_AUTO_ATTACH  "true"
         eval (zellij setup --generate-auto-start fish | string collect)
     end
+    
+    # Enable starship
     starship init fish | source
+
+    if test (uname) = "Darwin)
+        set -x SSH_AUTH_SOCK "/Users/
+    
 end
 
 # Replicate bash !!
